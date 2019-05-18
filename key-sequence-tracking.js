0b1d0349b66b23b2b50b3d75d6c14d8e5df9e474
PSJBit <patricksebastiandesign@gmail.com>
2019-05-18 22:10:34 +1000

    console logging

--- key-sequence-tracking.js
+++ key-sequence-tracking.js
@@ -1,13 +1,12 @@
 const pressed = [];
-const secretCode = 'wesbos';
+const secretCode = 'itsasecret';
 
 window.addEventListener('keyup', (e) => {
   console.log(e.key);
   pressed.push(e.key);
   pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
   if (pressed.join('').includes(secretCode)) {
-    console.log('DING DING!');
-    cornify_add();
+    console.warn('secret code found')
   }
   console.log(pressed);
 });
